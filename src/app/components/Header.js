afae2a61d2935c6af7ab4691b0e5a7fd72cec882
Gayathra Samuditha Vithanage Nanayakkara <gayathrasamuditha45@gmail.com>
2025-11-29 16:08:03 +0530

    Revert to regular paths, rely on client-side routing

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,7 +3,7 @@ import MediaIcon from "./MediaIcon";
 
 const base = "/Gayathra_Portfolio";
 
-const paths = [`${base}/#/`, `${base}/#/projects`, `${base}/#/about-me`];
+const paths = [`${base}/`, `${base}/projects`, `${base}/about-me`];
 
 export default (t) => {
     return /*html*/ `
@@ -29,15 +29,16 @@ export default (t) => {
                     </a>
                     <div class="header__links">
                        ${paths
-                        .map((path) => {
-                              const routeKey = path.replace(base, "").replace("#", "") || "/";
-                               return /*html*/ `
-                               <a href="${path}" class="header__link ${
-                                   window.location.hash === path ? "header__link_active" : ""
-                                }">${t[routes[routeKey].name]}</a>
-                              `;   
-                        })      
+                         .map((path) => {
+                             const routeKey = path.replace(base, "") || "/";
+                            return /*html*/ `
+                             <a href="${path}" class="header__link ${
+                               window.location.pathname === path ? "header__link_active" : ""
+                           }">${t[routes[routeKey].name]}</a>
+                          `;
+                      })
                       .join("")}
+
                     </div>
                     <div class="dropdown">
                         <span class="dropdown__label">en</span>
