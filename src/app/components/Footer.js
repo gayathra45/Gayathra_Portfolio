12cfc529a6b48bdf1e65bc98d280cb5336a3c9db
Gayathra Samuditha Vithanage Nanayakkara <gayathrasamuditha45@gmail.com>
2025-11-29 16:52:14 +0530

    Restore logo in footer, keep email removed

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,22 +2,35 @@ import MediaIcon from "./MediaIcon";
 import media from "@/consts/media";
 
 export default (t) => {
-    return /*html*/ `
+  return /*html*/ `
         <footer class="footer">
             <div class="container">
                 <div class="footer__inner">
-                    <div class="footer__info">                         
-                            <a class="footer__email" href="${media.email}">${media.emailRaw}</a>
-                        
-                        <p class="footer__description">${t.description}</p>
-                    </div>
+                    <div class="footer__info">
+    <div class="footer__header">
+        <div class="logo">
+            <img src="images/logo.svg" alt="logo" class="logo__img">
+            <div class="logo__name">Gayathra</div>
+        </div>
+    </div>
+    
+    <p class="footer__description">${t.description}</p>
+</div>
+
                     <div class="footer__media">
                         <div class="footer__title">${t.media}</div>
                         <div class="footer__list">
-                            ${["linkedin", "github", "twitter", "facebook", "instagram", "email"]
+                            ${[
+                              "linkedin",
+                              "github",
+                              "twitter",
+                              "facebook",
+                              "instagram",
+                              "email",
+                            ]
 
-                                .map((name) => MediaIcon({ name }))
-                                .join("")}
+                              .map((name) => MediaIcon({ name }))
+                              .join("")}
                         </div>
                     </div>
                 </div>
